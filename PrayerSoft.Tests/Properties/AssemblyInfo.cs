a3380468de130c5da8fb60d87ea514c0693d780b
mihai stancu <stancu.mihai.dorian@gmail.com>
2021-04-25 12:35:55 +0200

    Added unit tests project

---
+++ PrayerSoft.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PrayerSoft.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrayerSoft.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f73aeb09-f480-431f-acdf-d47ae319fb60")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PrayerSoft.Tests/UnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrayerSoft.Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void OnRefreshCurrentTimeIsDisplayed()
+        {
+            
+        }
+    }
+}
