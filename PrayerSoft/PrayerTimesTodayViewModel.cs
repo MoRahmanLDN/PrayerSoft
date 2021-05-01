fda164350619bb67737f078fd289d7bca14fbc8d
mihai stancu <stancu.mihai.dorian@gmail.com>
2021-05-01 13:28:24 +0300

    Improve code formatting

--- PrayerSoft/PrayerTimesTodayViewModel.cs
+++ PrayerSoft/PrayerTimesTodayViewModel.cs
@@ -16,25 +16,32 @@ namespace PrayerSoft.Tests
         }
 
         public string FajrBegins { get; set; }
-        public string AsrBegins { get; set; }
-        public string MaghribBegins { get; set; }
-        public string IshaBegins { get; set; }
         public string FajrJamaat { get; set; }
+
+        public string AsrBegins { get; set; }
         public string AsrJamaat { get; set; }
+
+        public string MaghribBegins { get; set; }
         public string MaghribJamaat { get; set; }
+
+        public string IshaBegins { get; set; }
         public string IshaJamaat { get; set; }
 
         public void Refresh()
         {
             DateTime today = clock.Read();
             var prayerTimes = repository.Get(today);
+            
             FajrBegins = format.ShortTime(prayerTimes.FajrBegins);
-            AsrBegins = format.ShortTime(prayerTimes.AsrBegins);
-            MaghribBegins = format.ShortTime(prayerTimes.MaghribBegins);
-            IshaBegins = format.ShortTime(prayerTimes.IshaBegins);
             FajrJamaat = format.ShortTime(prayerTimes.FajrJamaat);
+
+            AsrBegins = format.ShortTime(prayerTimes.AsrBegins);
             AsrJamaat = format.ShortTime(prayerTimes.AsrJamaat);
+
+            MaghribBegins = format.ShortTime(prayerTimes.MaghribBegins);
             MaghribJamaat = format.ShortTime(prayerTimes.MaghribJamaat);
+
+            IshaBegins = format.ShortTime(prayerTimes.IshaBegins);
             IshaJamaat = format.ShortTime(prayerTimes.IshaJamaat);
         }
     }
