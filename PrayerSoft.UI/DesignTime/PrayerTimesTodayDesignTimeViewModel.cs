531e7d5b6cbbf6009b426669163ef893a3c76a61
mihai stancu <stancu.mihai.dorian@gmail.com>
2021-05-01 21:37:30 +0300

    Fix design-time data

--- PrayerSoft.UI/DesignTime/PrayerTimesTodayDesignTimeViewModel.cs
+++ PrayerSoft.UI/DesignTime/PrayerTimesTodayDesignTimeViewModel.cs
@@ -4,14 +4,17 @@
     {
         public string FajrBegins { get; set; } = "10:10";
         public string FajrJamaat { get; set; } = "11:11";
+        
+        public string ZuhrBegins { get; set; } = "12:12";
+        public string ZuhrJamaat { get; set; } = "13:13";
 
-        public string AsrBegins { get; set; } = "12:12";
-        public string AsrJamaat { get; set; } = "13:13";
+        public string AsrBegins { get; set; } = "14:14";
+        public string AsrJamaat { get; set; } = "15:15";
 
-        public string MaghribBegins { get; set; } = "14:14";
-        public string MaghribJamaat { get; set; } = "15:15";
+        public string MaghribBegins { get; set; } = "16:16";
+        public string MaghribJamaat { get; set; } = "17:17";
 
-        public string IshaBegins { get; set; } = "16:16";
-        public string IshaJamaat { get; set; } = "17:17";
+        public string IshaBegins { get; set; } = "18:18";
+        public string IshaJamaat { get; set; } = "19:19";
     }
 }
