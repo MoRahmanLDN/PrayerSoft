da8510c212f1cf03807b0968f746c6954ff939e8
ubuntucsharp <ubuntucsharp@gmail.com>
2021-09-27 01:16:16 +0200

    Started making styles configurable

---
+++ PrayerSoft.UI/StyleConfig.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PrayerSoft.UI
+{
+    public class StyleConfig
+    {
+        private static JObject style;
+
+        public static Color BackgroundColor { get; set; }
+        public static Color TextColor { get; set; }
+        public static Color SplitterColor { get; set; }
+
+        public static FontFamily MessagesFontFamily { get; set; }
+        public static double MessagesFontSize { get; set; }
+        public static FontWeight MessagesFontWeight { get; set; }
+
+        public static FontFamily MosqueNameFontFamily { get; set; }
+        public static double MosqueNameFontSize { get; set; }
+        public static FontWeight MosqueNameFontWeight { get; set; }
+
+        static StyleConfig()
+        {
+            Load();
+        }
+
+        public static void Load()
+        {
+            style = JObject.Parse(File.ReadAllText("style.json"));
+
+            BackgroundColor = GetColor("BackgroundColor");
+            TextColor = GetColor("TextColor");
+            SplitterColor = GetColor("SplitterColor");
+
+            MessagesFontFamily = GetFont("MessagesFontFamily");
+            MessagesFontSize = GetDouble("MessagesFontSize");
+            MessagesFontWeight = GetFontWeight("MessagesFontWeight");
+
+            MosqueNameFontFamily = GetFont("MosqueNameFontFamily");
+            MosqueNameFontSize = GetDouble("MosqueNameFontSize");
+            MosqueNameFontWeight = GetFontWeight("MosqueNameFontWeight");
+        }
+
+        private static Color GetColor(string name)
+        {
+            return (Color)ColorConverter.ConvertFromString(style.Value<string>(name));
+        }
+
+        private static FontFamily GetFont(string name)
+        {
+            return new FontFamily(style.Value<string>(name));
+        }
+
+        private static double GetDouble(string name)
+        {
+            return style.Value<double>(name);
+        }
+
+        private static FontWeightConverter fontWeightConverter = new FontWeightConverter();
+
+        private static FontWeight GetFontWeight(string name)
+        {
+            return (FontWeight)fontWeightConverter.ConvertFromString(style.Value<string>(name));
+        }
+    }
+}
